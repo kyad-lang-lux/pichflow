921e8ebdf234f89bba25a5c37080d0b9fe66d602
kyad-lang-lux <emrissoglo@gmail.com>
2026-04-26 01:01:07 +0200

    jefj

--- src/lib/fedapay.ts
+++ src/lib/fedapay.ts
@@ -1,9 +1,5 @@
 import { FedaPay, Transaction } from 'fedapay';
 
-// Initialisation avec tes variables d'environnement
 FedaPay.setApiKey(process.env.FEDAPAY_SECRET_KEY!);
-
-// On utilise 'sandbox' pour tes tests, tu changeras en 'live' plus tard
-FedaPay.setEnvironment('sandbox'); 
-
+FedaPay.setEnvironment('sandbox');
 export { Transaction };
